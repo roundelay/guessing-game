7e3a4fc401c66b15b70823976b31de34246aa464
Marnie Cannon <marnie25@gmail.com>
2015-11-07 11:18:04 -0800

    Updataed game.js to include a fourth question, and hopefully improved code readability. Also added a blank README file.

--- game.js
+++ game.js
@@ -1,24 +1,53 @@
-var banjo = prompt('Can I play the banjo?');
+
+// Question 1
+/*var banjo = prompt('Can I play the banjo?');
+banjo = banjo.toLowerCase();
 console.log("banjo variable set to " + banjo);
-if (banjo === "yes" || banjo === "y" || banjo === "Yes" || banjo === "Y"|| banjo === "YES") {
-	alert("Sorry, that is incorrect!");
-}
-else {
+
+if (banjo === "no" || banjo === "n") {
 	alert("Excellent guess! I can't play the banjo, but I can play a little guitar.");
 }
+else {
+	alert("Sorry, that is incorrect!");
+}
+
+//Question 2
 var home = prompt('Did I grow up in the Napa Valley?');
+home = home.toLowerCase();
 console.log("home variable set to " + home);
-if (home === "yes" || home === "y" || home === "Yes" || home === "Y"|| home === "YES") {
+
+if (home === "yes" || home === "y") {
 	alert("You are correct! Have a glass of wine to celebrate.");
 }
 else {
 	alert("Sorry, that is not the right answer. No wine for you.");
 }
+
+//Question 3
 var comics = prompt('Do I like Marvel comics better than DC comics?');
+comics = comics.toLowerCase();
 console.log("comics variable set to " + comics);
-if (comics === "yes" || comics === "y" || comics === "Yes" || comics === "Y" || comics === "YES") {
+
+if (comics === "yes" || comics === "y") {
 	alert("Good job! Captain America salutes you!");
 }
 else {
 	alert("You have chosen... poorly. For your foolishness, Thor bops you on the head with his hammer.");
 } 
+*/
+//Question 4
+var jerseyNumber = prompt('What is my number on my softball jersey?');
+console.log('jerseyNumber variable set to ' + jerseyNumber);
+
+if (jerseyNumber == 25) {
+	alert('That is correct! Great guess!');
+}
+	else if (jerseyNumber > 25) {
+		alert("Sorry that guess is too high");
+	}
+	else if (jerseyNumber < 25) {
+		alert('Nope. Your guess is too low!');
+	}
+else {
+	alert('I\'m sorry, but I don\'t think you entered a valid number.');
+}
